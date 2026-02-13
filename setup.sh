759004fc59af5dc20f771cf909dcb01ed2e3b222
Matt Astill <matt@surianming.co.uk>
2026-02-13 16:57:06 +0000

    Adding customer yaml at demo stage

--- setup.sh
+++ setup.sh
@@ -3,14 +3,17 @@
 # remove kind cluster if exists for clean start
 kind delete cluster -n k8s-playground
 
+# mount drive we're going to hook into from kind
+sudo mount -t nfs4 127.0.0.1:/ /mnt/nfs
+
 # kind initialisation
 kind create cluster --config kind-config.yaml
 
-# nginx setup....
-./setup-nginx.sh
-
 # monitoring setup (prometheus and grafana)....
 ./setup-monitoring.sh
+
+# nginx setup....
+./setup-nginx.sh
 
 # kafka setup....
 ./setup-kafka.sh
